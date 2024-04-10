1fa4910156bdb5803ef0c9630838fb8ff661c1e9
DuyKhanhhhh <vuduykhanh14022004@gmail.com>
2024-04-10 09:12:25 +0700

    CallData

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './pages/Login/Login'
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home/Home";
+function App() {
+  return (
+      <>
+          <Routes>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/home' element={<Home/>}/>
+          </Routes>
+      </>
+  );
+}
+
+export default App;
